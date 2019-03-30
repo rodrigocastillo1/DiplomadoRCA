d35ab369220d56b9e2790176fef72d73797ab1ab
Rodrigo Castillo Alcántara <castillo_15zs@hotmail.com>
2019-03-30 12:29:11 -0600

    Práctica 5. Uso de sensores con ROS. Práctica 6. Lectura de encoders

---
+++ Practica05/EncodersRos/EncodersRos.ino
@@ -0,0 +1,126 @@
+#define RH_ENCODER_A 3
+#define RH_ENCODER_B 12
+
+int MotI = 12;         // Derecha giro adelante        |||||Mot1 -> motor derecho   Mot2 -> Motor izquierdo|||||
+int MotI_n = 13;       // Derecha giro atrás
+int MotD = 5;         // Izquierda giro adelante
+int MotD_n = 6;       // Izquierda giro atrás
+int pwmPin1 = 11;     // Señal PWM para motor 1
+int pwmPin2 = 10;     // Señal PWM para motor 2
+
+volatile long rightCount = 0;
+
+void rightEncoderEvent(){
+  if(digitalRead(RH_ENCODER_A) == HIGH){
+    if(digitalRead(RH_ENCODER_B) == LOW){
+      rightCount++;
+    }
+    else{
+      rightCount--;
+    }
+  }
+  else{
+    if(digitalRead(RH_ENCODER_B) == LOW){
+      rightCount--;
+    }
+    else{
+      rightCount++;
+    }
+  }
+}
+
+ros::NodeHandle nh;
+
+std_msgs::Int16 sharpSensor1;
+std_msgs::Int16 sharpSensor2;
+std_msgs::Bool contactSensor1;
+std_msgs::Bool contactSensor2;
+std_msgs::Int64 encoder1;
+std_msgs::Int64 encoder2;
+std_msgs::Int16 photo1;
+std_msgs::Int16 photo2;
+std_msgs::Int16 photo3;
+std_msgs::Int16 photo4;
+
+ros::Publisher sharpSensorPub1("/sharp_sensor_1", &sharpSensor1);
+ros::Publisher sharpSensorPub2("/sharp_sensor_2", &sharpSensor2);
+ros::Publisher contactSensorPub1("/contact_sensor_1", &contactSensor1);
+ros::Publisher contactSensorPub2("/contact_sensor_2", &contactSensor1);
+ros::Publisher encoderPub1("/encoder_1", &encoder1);
+ros::Publisher encoderPub2("/encoder_2", &encoder2);
+ros::Publisher photoPub1("/photo_1", &photo1);
+ros::Publisher photoPub2("/photo_2", &photo2);
+ros::Publisher photoPub3("/photo_3", &photo3);
+ros::Publisher photoPub4("/photo_4", &photo4);
+
+void speedMotor1Callback(const std_msgs::Float32& mess){
+  if(mess.data >= 0){
+    digitalWrite(MotI, HIGH);
+    digitalWrite(MotI_n, LOW);
+    analogWrite(pwmPin1, mess.data);
+  }
+  else{
+    digitalWrite(MotI, LOW);
+    digitalWrite(MotI_n, HIGH);
+    analogWrite(pwmPin1, fabs(mess.data));
+  }
+}
+
+void speedMotor2Callback(const std_msgs::Float32& mess){
+  if(mess.data >= 0){
+    digitalWrite(MotI, HIGH);
+    digitalWrite(MotI_n, LOW);
+    analogWrite(pwmPin1, mess.data);
+  }
+  else{
+    digitalWrite(MotI, LOW);
+    digitalWrite(MotI_n, HIGH);
+    analogWrite(pwmPin1, fabs(mess.data));
+}
+
+ros::Subscribe<std_msgs::Float32> subSpeedMotor1("/speed_motor_1", speedMotor1Callback);
+ros::Subscribe<std_msgs::Float32> subSpeedMotor2("/speed_motor_2", speedMotor2Callback);
+
+void setup() {
+  nh.initNode();
+  nh.advertise(sharpSensorPub1);
+  nh.advertise(sharpSensorPub2);
+  nh.advertise(contactSensorPub1);
+  nh.advertise(contactSensorPub2);
+  nh.advertise(encoderPub1);
+  nh.advertise(encoderPub2);
+  nh.advertise(photoPub1);
+  nh.advertise(photoPub2);
+  nh.advertise(photoPub3);
+  nh.advertise(photoPub4);
+  nh.subscribe(subsSpeedMotor1);
+  nh.subscribe(subsSpeedMotor2);
+  pinMode(MotI, OUTPUT);
+  pinMode(MotI_n, OUTPUT);
+
+}
+
+void loop() {
+  sharpSensor1.data = 0.5;
+  sharpSensor2.data = 0.7;
+  contactSensor1.data = true;
+  contactSensor2.data = false;
+  encoder1.data = rightCount;
+  encoder2.data = 0;
+  photo1.data = 1;
+  photo2.data = 1;
+  photo3.data = 1;
+  photo4.data = 1;
+  sharpSensorPub1.publish(&sharpSensor1);
+  sharpSensorPub2.publish(&sharpSensor2);
+  contactSensorPub1.publish(&contactSensor1);
+  contactSensorPub2.publish(&contactSensor2);
+  encoderPub2.publish(&encoder1);
+  encoderPub2.publish(&encoder2);
+  photoPub1.publish(&photo1);
+  photoPub2.publish(&photo2);
+  photoPub3.publish(&photo3);
+  photoPub4.publish(&photo4);
+  nh.spinOnce();
+  delay(50);
+}
---
+++ Practica06/VelocidadEncoders/VelocidadEncoders.ino
@@ -0,0 +1,86 @@
+int motD_A_pin = 3;
+int motD_B_pin = 4;
+int motI_A_pin = 2;
+int motI_B_pin = 1;
+
+int MotI = 12;         // Derecha giro adelante        |||||Mot1 -> motor derecho   Mot2 -> Motor izquierdo|||||
+int MotI_n = 13;       // Derecha giro atrás
+int MotD = 5;         // Izquierda giro adelante
+int MotD_n = 6;       // Izquierda giro atrás
+int pwmPin1 = 11;     // Señal PWM para motor 1
+int pwmPin2 = 10;     // Señal PWM para motor 2
+
+int D_sA;
+int D_sB;
+int I_sA;
+int I_sB;
+
+unsigned long currentPeriod = 0;
+unsigned long currentTime = 0;
+unsigned long startTime = 0;
+unsigned long period = 1000L*1000;
+
+volatile int DcountA = 0;
+volatile int DcountB = 0;
+volatile int IcountA = 0;
+volatile int IcountB = 0;
+float velD = 0;
+float velI = 0;
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(motD_A_pin), incDA, RISING);
+  attachInterrupt(digitalPinToInterrupt(motI_A_pin), incIA, RISING);
+  pinMode(motD_A_pin, INPUT);
+  pinMode(motD_B_pin, INPUT);
+  pinMode(motI_A_pin, INPUT);
+  pinMode(motI_B_pin, INPUT);
+
+  digitalWrite(MotI, HIGH);
+  digitalWrite(MotI_n, LOW);
+  digitalWrite(MotD, HIGH);
+  digitalWrite(MotD_n, LOW);
+  analogWrite(pwmPin1, 30);
+  analogWrite(pwmPin2, 35);
+}
+
+void loop() {
+  //checkEncoders();
+  currentTime = micros();
+  Serial.println((float)currentTime);
+  currentPeriod = currentTime - startTime;
+  if(currentPeriod >= period){
+    Serial.println(IcountA);
+    Serial.println((float)currentPeriod/1000000);
+    velD = (float)(DcountA/(currentPeriod/1000000));     //Velocidad en giros por segundo
+    velI = (float)(IcountA/(currentPeriod/1000000));     //Velocidad en giros por segundo
+    DcountA = 0;
+    //DcountB = 0;
+    IcountA = 0;
+    //IcountB = 0;
+    startTime = currentTime;
+  }
+  Serial.print("Velocidad motor derecho: ");
+  Serial.println(velD);
+  Serial.print("Velocidad motor izquierdo: ");
+  Serial.println(velI);
+}
+
+void incDA(){
+  DcountA+=1;
+}
+
+void incIA(){
+  IcountA+=1;
+}
+
+/*void checkEncoders(){
+  if(digitalRead(motD_A_pin) == 1)
+    DcountA+=1;
+  if(digitalRead(motD_B_pin) == 1)
+    DcountB+=1;
+  if(digitalRead(motI_A_pin) == 1)
+    IcountA+=1;
+  if(digitalRead(motI_B_pin) == 1)
+    IcountB+=1;
+}*/
